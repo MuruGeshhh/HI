06ae7b128c17dc208a0df784b5ca5ae4246c296b
MuruGeshhh <108484042+MuruGeshhh@users.noreply.github.com>
2022-09-23 01:42:22 +0800

    commit changessds

--- src/main/java/vttp/ssf/template/Controllers/MainController.java
+++ src/main/java/vttp/ssf/template/Controllers/MainController.java
@@ -3,17 +3,16 @@ package vttp.ssf.template.Controllers;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.http.HttpSession;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.MultiValueMap;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,7 +24,6 @@ import vttp.ssf.template.Models.Review;
 import vttp.ssf.template.Models.ReviewForm;
 import vttp.ssf.template.Models.SearchModel;
 import vttp.ssf.template.Models.Top25model;
-import vttp.ssf.template.Models.TrailerModel;
 import vttp.ssf.template.Service.ComingSoonService;
 import vttp.ssf.template.Service.MovieDetail;
 import vttp.ssf.template.Service.ReviewService;
@@ -60,13 +58,13 @@ public class MainController {
 
 
 
-    @RequestMapping(path="/")
+    @RequestMapping(value={"/", "/accept",""," "},method=RequestMethod.GET)
     @GetMapping
     public String getE(Model model){
 
         
         
-        return "/Main";
+        return "/index";
 
     }
 
